6979ccef33a8bed2d892b10eb36ec3b2028f8dc6
Grace Thompson <Grace.thompson17@aol.com>
2022-05-25 09:00:28 -0500

    errors with the last few fields

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -2,20 +2,37 @@ package org.launchcode.codingevents.models;
 
 import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class Event {
     private String name;
     private String description;
+
     @NotBlank(message="Location can't be left blank!")
     private String location;
+
+    @AssertTrue(message="All events must be registered for")
+    private  boolean mustRegister;
+
+    @Positive(message="Number of attendees must be one or more")
+    private int numAttendees;
+
+
     private int id;
     private static int nextId=1;
 
     public Event(String name, String description, String location){
+        this();
         this.name=name;
         this.description=description;
         this.location=location;
+
+    }
+    public Event(){
         this.id=nextId;
         nextId++;
     }
@@ -43,6 +60,14 @@ public class Event {
 
     public String getLocation() {
         return location;
+    }
+
+    public boolean isMustRegister() {
+        return mustRegister;
+    }
+
+    public int getNumAttendees() {
+        return numAttendees;
     }
 
     @Override
